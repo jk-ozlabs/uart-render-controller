08e854a6c425011d029e4e02241afee5060f15eb
Oskar Senft <osk@google.com>
2021-01-26 00:35:12 -0500

    Fix incorrect parsing of large values in VGA scratch register.
    The largest value in the 32 bit VGA scratch register is 0xffffffff,
    which is 4294967295 decimal. This requires a parsing buffer for 10
    digits + \0. This change increases the parsing buffer to char[12],
    and adds the trailing \0.
    
    Signed-off-by: Oskar Senft <osk@google.com>
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- uart-render-controller.c
+++ uart-render-controller.c
@@ -139,7 +139,8 @@ static void set_renderer_running(struct render_ctx *ctx, bool running)
 
 static int read_vga_pw_reg(struct render_ctx *ctx, uint8_t *regp)
 {
-	char buf[8], *endp;
+	char buf[12]; /* Cover 32 bit unsigned int as decimal + \n + \0 */
+	char *endp;
 	unsigned long tmp;
 	int rc;
 
@@ -149,11 +150,12 @@ static int read_vga_pw_reg(struct render_ctx *ctx, uint8_t *regp)
 		return -1;
 	}
 
-	rc = read(ctx->vga_pw_fd, buf, sizeof(buf));
+	rc = read(ctx->vga_pw_fd, buf, sizeof(buf) - 1);
 	if (rc < 1) {
 		warn("Can't read VGA scratch register");
 		return -1;
 	}
+	buf[rc] = '\0';
 
 	errno = 0;
 	tmp = strtoul(buf, &endp, 0);
